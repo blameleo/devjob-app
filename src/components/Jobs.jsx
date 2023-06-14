58850f3db3940ea884f1f9d9748ba95f504f7acb
leonard <leonardgre@gmail.com>
2023-06-14 15:17:56 +0000

    added logim and sign up page

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,30 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import data from '../data.json'
-import Job from './Job';
+import React, { useEffect, useState } from "react";
+import data from "../data.json";
+import Job from "./Job";
+import { db } from "../firebase";
+import {
+  Query,
+  QuerySnapshot,
+  collection,
+  onSnapshot,
+} from "firebase/firestore";
 
-const Jobs = ({setJobs,setSearchResults,searchResults}) => {
+const Jobs = ({ setJobs, setSearchResults, searchResults }) => {
+  // useEffect(()=>{
+  //   setJobs(data)
+  //   setSearchResults(data)
+  //     },[])
 
+  useEffect(() => {
+    const q = Query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let jobs = [];
+      QuerySnapshot;
+    });
+  }, []);
 
-  useEffect(()=>{
-    setJobs(data)
-    setSearchResults(data)
-      },[])
-    
   return (
- 
-    <div className='grid sm:grid-cols-2 lg:grid-cols-3   w-10/12 mx-auto  '>
-      {
-        searchResults.map((job,index)=>{
-            return <Job key={index} data={job}/>
-        })
-      }
-
-
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3   w-10/12 mx-auto  ">
+      {searchResults.map((job, index) => {
+        return <Job key={index} data={job} />;
+      })}
     </div>
+  );
+};
 
-  
-
-  )
-}
-
-export default Jobs
+export default Jobs;
